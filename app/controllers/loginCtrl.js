8713f4c8167236759d4a2b7b1bfa796be42b6f33
uodyne <uodyne@gmail.com>
2014-09-14 18:34:08 +0200

    class active on dynamic content

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -4,18 +4,22 @@ define(['modules/app'], function(app)
 	{
 
 		$scope.templates=[{
+			id: 1,
 		    name:'First Course',
 		    url:'views/courses/course1.html'
 		  }
 		  ,{
+		  	id: 2,
 		    name:'Last Course',
 		    url:'views/courses/course2.html'
 		  }
 		];
 
 		$scope.selectedTemplate=$scope.templates[0].url;
+		$scope.selectedTemplateId=$scope.templates[0].id;
 		$scope.selectTemplate=function(template){
 			$scope.selectedTemplate=template.url;
+			$scope.selectedTemplateId=template.id;
 		}
 	}]);
 });
