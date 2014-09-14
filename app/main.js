4a71c3a8369ecdd312a32fb5d77673b2102806d9
uodyne <uodyne@gmail.com>
2014-09-14 15:52:42 +0200

    added domReady
    But…

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ require.config({
 	paths: {
 		'angular': './components/angular/angular',
 		'angular-resource': './components/angular-resource/angular-resource',
+		'domReady': './components/requirejs-domready/domReady',
 		'ui.router': './components/angular-ui-router/release/angular-ui-router'
 	},
 	shim: {
@@ -11,12 +12,14 @@ require.config({
 		'angular-resource': { deps: ['angular']}
 	}
 });
+	
 
-require(['angular', 'routes/routes'], function(angular, mainRoutes)
+
+require(['angular', 'routes/routes', 'domReady'], function(angular, mainRoutes, domReady)
 {
-	setTimeout(function() {
+    domReady(function () {
 		document.getElementById( 'appContainer' ).className = 'loader';
 		document.getElementById( 'appLoading' ).className = 'loader';
-	},100);
+    });
 	return angular.bootstrap(document, ["app"]);
 });
--- app/modules/app.js
+++ app/modules/app.js
@@ -1,4 +1,4 @@
-define(['angular', 'ui.router', 'angular-resource'], function(angular)
+define(['angular', 'ui.router', 'angular-resource'], function(angular,domReady)
 {
 	return angular.module('app', ['ngResource', 'ui.router']);
 });
